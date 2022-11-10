522e8b53efc2399d44dd7f90435c3a8091ce69d5
Loy502 <112780304+Loy502@users.noreply.github.com>
2022-11-10 19:26:52 +0900

    Add files via upload

---
+++ html/html/06retouching/js/click.js
@@ -0,0 +1,44 @@
+
+//클릭 시 이미지 바꾸기
+$(function() {
+    $('.retouching_click').click(function() {
+        $('.original_click').show().siblings('div').hide();
+    })
+});
+$(function() {
+    $('.original_click').click(function() {
+        $('.retouching_click').show().siblings('div').hide();
+    })
+});
+
+
+
+
+
+//클릭 시 이미지 바꾸기
+$(function() {
+    $('.retouching_click1').click(function() {
+        $('.original_click1').show().siblings('div').hide();
+    })
+});
+$(function() {
+    $('.original_click1').click(function() {
+        $('.retouching_click1').show().siblings('div').hide();
+    })
+});
+
+
+
+
+
+//클릭 시 이미지 바꾸기
+$(function() {
+    $('.retouching_click2').click(function() {
+        $('.original_click2').show().siblings('div').hide();
+    })
+});
+$(function() {
+    $('.original_click2').click(function() {
+        $('.retouching_click2').show().siblings('div').hide();
+    })
+});
