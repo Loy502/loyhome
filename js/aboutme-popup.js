c18bc384778a0df763a345624ac79d6d5ffb70ae
Loy502 <112780304+Loy502@users.noreply.github.com>
2024-07-01 18:09:28 +0900

    Add files via upload

--- js/aboutme-popup.js
+++ js/aboutme-popup.js
@@ -18,7 +18,12 @@ $(document).ready(function () {
 
 
 
-//팝업 아래 스크롤 막기
+//자기소개서 레이어 팝업 종료 시 아래 스크롤 상단 위치
+$("#pop_content").
+    modal.scrollTop; // O
+modal.scrollTop(0); // X
+modal.scrollTo(0, 0);  // O
+modal.animate({ scrollTop: 0 });  // X
 
 
 //자기소개서 레이어 팝업 호출 시 바디 스크롤 방지 참고 링크 - 
