551d2135f12ddec678d1ee0fd9cdd4ebdab505cb
Loy502 <112780304+Loy502@users.noreply.github.com>
2024-07-01 17:20:53 +0900

    Add files via upload

--- js/00_0-scrollbar.js
+++ js/00_0-scrollbar.js
@@ -1,1 +1,5 @@
-history.scrollRestoration = "auto" //새로고침 스크롤 위치 manual = 복원 안함 / auto  = 복원 < 익스플로러 12부터 적용
+history.scrollRestoration = "manual";
+//새로고침 시 항상 top:0 위치로 이동된다.
+
+// history.scrollRestoration = "auto";
+//현재 위치를 기억하여 새로고침시 그자리를 유지한다.
